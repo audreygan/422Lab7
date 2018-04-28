3ddf243cad3da0091f884d2005f823e27363a66c
Unknown <audreygan@utexas.edu>
2018-04-28 15:28:02 -0500

    right phrases and correct user input

--- src/Main.java
+++ src/Main.java
@@ -11,30 +11,30 @@ static Scanner kb;	// scanner connected to keyboard input, or input file
         while(input.size() != 0) {//keep running if "quit" command not entered
             if(input.size() == 1){} //if exception was encountered then keep running, not a "quit"
             else{
-                String rightFilePath = input.get(input.size()-2);
-                if(!input.get(input.size()-2).endsWith("/")){   //account for if file path doesn't end with a "/"
-                    input.remove(input.size()-2);
+                String rightFilePath = input.get(input.size()-3);
+                if(!input.get(input.size()-3).endsWith("/")){   //account for if file path doesn't end with a "/"
+                    input.remove(input.size()-3);
                     rightFilePath += "/";
-                    input.add(input.size()-1, rightFilePath);
+                    input.add(input.size()-2, rightFilePath);
                 }
-                int phraseLen = Integer.valueOf(input.get(input.size()-1));
+                int phraseLen = Integer.valueOf(input.get(input.size()-2));
                 HashMap<String, ArrayList<ArrayList<String>>> hm = new HashMap<>();
-                for(int i = 0; i<input.size()-3; i++) {//goes until the second to last file
+                for(int i = 0; i<input.size()-3; i++) {//goes until the last file
                     ArrayList<ArrayList<String>> phrases = new ArrayList<>();
                     File thisFile = new File(rightFilePath + input.get(i));   //the actual test doc to compare against all others
-                    String line1;
+//                    String line1;
 //                    System.out.println(input.get(i)); //*********DEBUG STATEMENT making sure all files are being accessed*********
                     try{
                         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(thisFile), "UTF-8"));
                         Scanner s = new Scanner(br);    //make a scanner that uses the buffered reader to read the file
                         while (s.hasNext()) {
-                            int counter = 0;
+                            int numWords = 0;
                             ArrayList<String> stringEntry = new ArrayList<>();
-                            while((counter < phraseLen) && (s.hasNext()) && (hm.size() == 0)){
+                            while((numWords < phraseLen) && (s.hasNext()) && (phrases.size() == 0)){
                                 stringEntry.add(s.next().replaceAll("[^\\w\\s\\ ]", "").toLowerCase()); //get the first phrase in the doc
-                                counter++;
+                                numWords++;
                             }
-                            if((hm.size()>0) && (s.hasNext())){
+                            if((phrases.size()>0) && (s.hasNext())){
                                 ArrayList<String> newPhrase = new ArrayList<>(phrases.get(phrases.size()-1));     //for all other phrases, get the last phrase
                                 newPhrase.remove(0);                                                        //remove the first word
                                 newPhrase.add(s.next().replaceAll("[^\\w\\s\\ ]", "").toLowerCase());//add the next word
@@ -61,45 +61,6 @@ static Scanner kb;	// scanner connected to keyboard input, or input file
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    for (int j = i + 1; j < input.size() - 2; j++) {//goes until the last file
-                        File otherFile = new File(rightFilePath + input.get(j));    //file to be compared against
-                        int k = 0; int w = 0;
-                        String words1[]; String words2[];
-                        String line2;
-                        ArrayList<ArrayList<String>> commonPhrases = new ArrayList<>();
-//                        try {
-//                            BufferedReader br2 = new BufferedReader(new InputStreamReader(new FileInputStream(otherFile), "UTF-8"));
-//                            line2 = br2.readLine();
-//
-//    //                        while((line1 != null)){ //while you haven't reached the end of first file
-//    //                            words1 = line1.split("\\s");    //get the words for each line in file 1
-//    //                            words2 = line2.split("\\s");    //get the words for each line in file 2
-//    //                            if(words1[k].equals(words2[w])){    //encountered the same word
-//    //                                phrases.add(words1[k]);
-//    //                                counter++;
-//    //                                k++;
-//    //                                w++;
-//    //                            }else{  //end of phrase match, add to list of common phrases between this file and other file
-//    //                                if(counter >= phraseLen){   //makes sure added phrases are of at least the required phrase length
-//    //                                    commonPhrases.add(phrases);
-//    //                                }
-//    //                                phrases.clear();
-//    //                                counter = 0; w++;
-//    //                            }
-//    ////                            if (hm.containsKey(words1[0])){
-//    ////                                System.out.println("Found duplicate ... handle logic");
-//    ////                            }
-//    ////                            hm.put(words1[0],words1[1]); //if index==0 is ur key
-//    //                            line1 = br.readLine();
-//    //                            line2 = br.readLine();
-//    //                        }
-//
-//                        } catch (FileNotFoundException e) {
-//                            e.printStackTrace();
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-                    }
                 }
             }
             input = parse(kb);  //run the program until quit is entered
@@ -123,9 +84,15 @@ static Scanner kb;	// scanner connected to keyboard input, or input file
         if((command[0].equals(quit)) && (command.length == 1)){ //valid quit command
             return input;
         }
+        if(command.length > 3){
+            input.add("dummy");
+            System.out.println("Invalid input. Try again.");
+            return input;
+        }
         String filePath = command[0];
         try{
             int second = Integer.valueOf(command[1]);
+            int third = Integer.valueOf(command[2]);
             File folder = new File(filePath);
             String[] srcFiles = folder.list();  //get the list of input files
             for(int i = 0; i<srcFiles.length; i++){
@@ -133,6 +100,7 @@ static Scanner kb;	// scanner connected to keyboard input, or input file
             }
             input.add(command[0]);  //add the filepath in input so you can access the other files
             input.add(command[1]);  //add the phrase length in input so you can access it later
+            input.add(command[2]);  //add the minimum phrase matches to input
         }catch(NumberFormatException e){    //makes sure that the phrase length is an integer
             input.add("dummy");
             System.out.println("Invalid input: Phrase length parameter incorrect. Try again.");
